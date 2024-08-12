3943f91ae778a1944562974a078bd60e17563e8b
steward-fu <steward.fu@gmail.com>
2024-08-12 09:58:53 +0800

    ObjAsm Refined.

--- objasm/Examples/DDK/WDM/Hello/main.asm
+++ objasm/Examples/DDK/WDM/Hello/main.asm
@@ -1,22 +1,29 @@
+;;
+;; Author: Steward Fu
+;; Update: 2024/08/12
+;;
+;; Hello example based on WDM model
+;;
+
 %include @Environ(OBJASM_PATH)/Code/Macros/Model.inc
+
 SysSetup OOP, DDK32, ANSI_STRING
 
-MakeObjects Primer
-MakeObjects KDriver
-MakeObjects KDeviceBase, KDevice, KPnpDevice
-MakeObjects KLowerDevice, KPnpLowerDevice
+MakeObjects Primer, KDriver, KPnpDevice, KPnpLowerDevice
 
     .const
 Object MyDriver, , KDriver
     RedefineMethod Init
+    RedefineMethod Delete
     RedefineMethod AddDevice, PDEVICE_OBJECT
     RedefineMethod DriverEntry, PUNICODE_STRING
     RedefineMethod DriverIrpDispatch, PIRP
 ObjectEnd
 
 Object MyDevice, , KPnpDevice
+    RedefineMethod Delete
     RedefineMethod Init, PDEVICE_OBJECT
-    RedefineMethod DefaultPnp, POINTER
+    RedefineMethod DefaultPnp, PKIrp
     RedefineMethod DeviceIrpDispatch, PIRP
 
     Embed m_pMyLowerDevice, KPnpLowerDevice
@@ -26,9 +33,15 @@ ObjectEnd
 DECLARE_DRIVER_CLASS MyDriver, $OfsCStr("MyDriver")
 
 Method MyDriver.Init, uses esi
-    D $OfsCStr("MyDriver.Init()")
+    D $OfsCStr("MyDriver.Init(this:0x%x)"), pSelf
 
     ACall pSelf::MyDriver.Init
+MethodEnd
+
+Method MyDriver.Delete, uses esi
+    D $OfsCStr("MyDriver.Delete(this:0x%x)"), pSelf
+
+    invoke ExFreePool, pSelf
 MethodEnd
 
 Method MyDriver.DriverEntry, uses esi, pMyRegistry : PUNICODE_STRING
@@ -39,7 +52,7 @@ MethodEnd
 
 Method MyDriver.AddDevice, uses esi, pPhyDevice : PDEVICE_OBJECT
     D $OfsCStr("MyDriver.AddDevice()")
-    T $OfsCStr("Hello, world\:")
+    T $OfsCStr("Hello, world\!")
 
     New MyDevice
     OCall eax::MyDevice.Init, pPhyDevice
@@ -56,12 +69,19 @@ Method MyDriver.DriverIrpDispatch, uses esi, pMyIrp : PIRP
 MethodEnd
 
 Method MyDevice.Init, uses esi, pPhyDevice : PDEVICE_OBJECT
-    D $OfsCStr("MyDevice.Init()")
+    D $OfsCStr("MyDevice.Init(this:0x%x)"), pSelf
 
-    ACall pSelf::MyDevice.Init, $OfsCStrW("\Device\MyDriver"), FILE_DEVICE_UNKNOWN, NULL, 0, DO_BUFFERED_IO
+    ACall Init, $OfsCStrW("\Device\MyDriver"), FILE_DEVICE_UNKNOWN, NULL, 0, DO_BUFFERED_IO
 
     SetObject esi
     OCall [esi].m_pMyLowerDevice::KPnpLowerDevice.Initialize, [esi].m_pMyDevice, pPhyDevice
+    OCall SetLowerDevice, addr [esi].m_pMyLowerDevice
+MethodEnd
+
+Method MyDevice.Delete, uses esi
+    D $OfsCStr("MyDevice.Delete(this:0x%x)"), pSelf
+
+    invoke ExFreePool, pSelf
 MethodEnd
 
 Method MyDevice.DeviceIrpDispatch, uses esi, pMyIrp : PIRP
@@ -70,7 +90,7 @@ Method MyDevice.DeviceIrpDispatch, uses esi, pMyIrp : PIRP
     ACall DeviceIrpDispatch, pMyIrp
 MethodEnd
 
-Method MyDevice.DefaultPnp, uses esi, I : POINTER
+Method MyDevice.DefaultPnp, uses esi, I : PKIrp
     D $OfsCStr("MyDevice.DefaultPnp()")
 
     OCall I::KIrp.ForceReuseOfCurrentStackLocationInCalldown
